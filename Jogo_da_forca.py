b9a5e657ec23c63b9dbc1032809b0609e4d96bbd
gugasantos <62771440+gugasantos@users.noreply.github.com>
2020-04-04 23:39:12 -0300

    Update Jogo_da_forca.py

--- Jogo_da_forca.py
+++ Jogo_da_forca.py
@@ -1,6 +1,13 @@
 import os
+from unicodedata import normalize
+
+
+def remover_acentos(txt):
+    return normalize('NFKD', txt).encode('ASCII', 'ignore').decode('ASCII')
+
 
 secreto = input("Escreva a palavra secreta: ").upper().strip()
+secreto = remover_acentos(secreto)
 os.system("cls")
 vetor = []
 a = 1
@@ -25,9 +32,30 @@ while True:
             break
 
     inp = input("Digite uma letra: ").upper().strip()
-    if len(inp) > 1:
+    inp = remover_acentos(inp)
+    if inp == secreto:
+        print(inp.upper())
+        print("Parabens você venceu!")
+        s = input("Quer tentar denovo?[S]im ou [N]ão ").upper().strip()
+        if s == 'S':
+            secreto = input("Escreva a palavra secreta: ").upper().strip()
+            os.system("cls")
+            vetor.clear()
+            saida = []
+            saida.clear()
+            a = 1
+            continue
+        elif s == 'N':
+            print("Foi bom jogar com você espero que volte noavamente ")
+            break
+        else:
+            print("Vai se fuder então, thau!")
+            break
+
+    elif len(inp) > 1:
         print("UMA letra apenas meu querido!")
         continue
+
     # 2
     vetor.append(inp)
     # 3
@@ -40,7 +68,7 @@ while True:
             saida.append(letra)
 
         else:
-            saida.append("_ ")
+            saida.append(" _")
     # 4
     verificador = "".join(saida)
     if secreto == verificador:
