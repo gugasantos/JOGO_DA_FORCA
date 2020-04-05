5d07087e74352df7b3c79463a82fbeffcfa045ed
gugasantos <62771440+gugasantos@users.noreply.github.com>
2020-04-04 23:04:51 -0300

    Create Jogo_da_forca.py

---
+++ Jogo_da_forca.py
@@ -0,0 +1,65 @@
+import os
+
+secreto = input("Escreva a palavra secreta: ").upper().strip()
+os.system("cls")
+vetor = []
+a = 1
+while True:
+    print("Você tem {} tentativas".format(6-a))
+    if a > 5:
+        print("Você perdeu")
+        s = input("Quer tentar denovo?[S]im ou [N]ão ").upper().strip()
+        if s == 'S':
+            secreto = input("Escreva a palavra secreta: ").upper().strip()
+            os.system("cls")
+            vetor.clear()
+            saida = []
+            saida.clear()
+            a = 1
+            continue
+        elif s == 'N':
+            print("Foi bom jogar com você espero que volte noavamente ")
+            break
+        else:
+            print("Vai se fuder então, thau!")
+            break
+
+    inp = input("Digite uma letra: ").upper().strip()
+    if len(inp) > 1:
+        print("UMA letra apenas meu querido!")
+        continue
+    # 2
+    vetor.append(inp)
+    # 3
+    if inp not in secreto:
+        a += 1
+    saida = []
+    # 1
+    for letra in secreto:
+        if letra in vetor:
+            saida.append(letra)
+
+        else:
+            saida.append("_ ")
+    # 4
+    verificador = "".join(saida)
+    if secreto == verificador:
+        print(verificador.upper())
+        print("Parabens você venceu!")
+        s = input("Quer tentar denovo?[S]im ou [N]ão ").upper().strip()
+        if s == 'S':
+            secreto = input("Escreva a palavra secreta: ").upper().strip()
+            os.system("cls")
+            vetor.clear()
+            saida = []
+            saida.clear()
+            a = 1
+            continue
+        elif s == 'N':
+            print("Foi bom jogar com você espero que volte noavamente ")
+            break
+        else:
+            print("Vai se fuder então, thau!")
+            break
+    os.system("cls")
+    print(verificador)
